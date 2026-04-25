a3dc99f260bc45318a8e344b60430cc9aa5f2a81
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-04-25 19:56:33 +0900

    feat: add info modal

--- src/features/header/info-button.tsx
+++ src/features/header/info-button.tsx
@@ -1,9 +1,13 @@
 import { ActionIcon } from "@mantine/core";
 import { IconInfoCircle } from "@tabler/icons-react";
 
-export const InfoButton = () => {
+type Props = {
+  onClick: () => void;
+};
+
+export const InfoButton = ({ onClick }: Props) => {
   return (
-    <ActionIcon variant="default" size="lg" aria-label="Pick Language">
+    <ActionIcon variant="default" size="lg" aria-label="Pick Language" onClick={onClick}>
       <IconInfoCircle style={{ height: "70%", width: "70%" }} stroke={1.5} />
     </ActionIcon>
   );
