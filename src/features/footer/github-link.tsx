8906a232918ec49ee5c5dfd91c6c1b5d4edfa8af
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-04-25 23:59:57 +0900

    fix: vulnerability

--- src/features/footer/github-link.tsx
+++ src/features/footer/github-link.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 export const GithubLink = () => {
   const { t } = useTranslation();
   return (
-    <a href="https://github.com/stuartstew/enchant-cost-optimizer" target="_blank" rel="noopener">
+    <a href="https://github.com/stuartstew/enchant-cost-optimizer" target="_blank" rel="noopener noreferrer">
       <Button leftSection={<IconBrandGithub size={16} />} variant="default" size="compact-md">
         {t("github-link")}
       </Button>
