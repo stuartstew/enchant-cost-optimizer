1b8c5b1ab02c65580bf268413526a59e482c4e22
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-04-30 21:57:05 +0900

    feat: make header responsive

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,25 +1,32 @@
-import { Group, Title } from "@mantine/core";
+import { Group, Stack } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { useTranslation } from "react-i18next";
+import { AppTitle } from "./app-title";
 import { ColorSchemeToggle } from "./color-scheme-toggle";
 import { InfoButton } from "./info-button";
 import { InfoModal } from "./info-modal";
 import { LanguagePicker } from "./language-picker";
 
 export const Header = () => {
-  const { t } = useTranslation();
   const [opened, { open, close }] = useDisclosure(false);
   return (
     <>
       <InfoModal opened={opened} onClose={close} />
-      <Group h="100%" px="md" justify="space-between">
-        <Title size={24}>{t("title")}</Title>
+      <Group visibleFrom="sm" h="100%" px="md" justify="space-between">
+        <AppTitle />
         <Group h="100%" gap="xs">
           <InfoButton onClick={open} />
           <LanguagePicker />
           <ColorSchemeToggle />
         </Group>
       </Group>
+      <Stack hiddenFrom="sm" h="100%" gap="md" px="md" justify="center">
+        <AppTitle />
+        <Group gap="xs">
+          <InfoButton onClick={open} />
+          <LanguagePicker />
+          <ColorSchemeToggle />
+        </Group>
+      </Stack>
     </>
   );
 };
--- src/shell.tsx
+++ src/shell.tsx
@@ -1,11 +1,16 @@
-import { AppShell, Divider } from "@mantine/core";
+import { AppShell, Divider, useMatches } from "@mantine/core";
 import { Footer } from "@/features/footer";
 import { Header } from "@/features/header";
 import { Main } from "@/features/main";
 
 export const Shell = () => {
+  const height = useMatches({
+    base: 120,
+    sm: 60,
+  });
+
   return (
-    <AppShell header={{ height: 60 }}>
+    <AppShell header={{ height }}>
       <AppShell.Header>
         <Header />
       </AppShell.Header>
